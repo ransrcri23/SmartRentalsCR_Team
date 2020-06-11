ba10770d32a6e1bbf4ac700930063ad7b1ec48e1
Pedro2424 <65990529+Pedro2424@users.noreply.github.com>
2020-06-10 21:49:44 -0600

    PRUEBA PEDRO

--- ProyectoSmartRentals/Modelos/SmR_Model.Designer.cs
+++ ProyectoSmartRentals/Modelos/SmR_Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Randall\Source\Repos\SmartRentalsCR\ProyectoSmartRentals\ProyectoSmartRentals\Modelos\SmR_Model.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\pedro_000\Source\Repos\ransrcri23\SmartRentalsCR_Team\ProyectoSmartRentals\Modelos\SmR_Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
